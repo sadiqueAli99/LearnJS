5286c0ff4ec0ca62a1875be387f5e835a9f8e521
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2024-02-17 08:51:30 +0400

    day2

---
+++ anagram.js
@@ -0,0 +1,5 @@
+function checkAnagram(firstString,secondString){
+    return firstString.split("").sort().join("")===secondString.split("").sort().join("");
+}
+
+console.log(checkAnagram('sadique','sadique'))
