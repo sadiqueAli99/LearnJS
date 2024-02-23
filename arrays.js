93c721e4864f8337f558ac0f2a0913f57f4f451a
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2024-02-23 11:12:25 +0400

    Formatted Table Output

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ const fruites =new Array('Apple' ,'banana','mango');
 
 console.log(fruites.length)
 fruites[3]='yango'
+fruites[fruites.length]='peach'
 console.log(fruites)
---
+++ formatted.js
@@ -0,0 +1,19 @@
+const data=[{
+    "name":"sadique",
+    "age":28,
+    "place":"Dubai"
+
+},
+{
+    "name":"sadique",
+    "age":28,
+    "place":"Dubai"
+
+},
+{
+    "name":"sadique",
+    "age":28,
+    "place":"Dubai"
+
+}];
+console.table(data);
