d81699bfa5bdb162b2c3b374d34603252fb9043f
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2024-02-23 11:33:13 +0400

    array Fun

--- arrays.js
+++ arrays.js
@@ -4,3 +4,20 @@ console.log(fruites.length)
 fruites[3]='yango'
 fruites[fruites.length]='peach'
 console.log(fruites)
+
+const arr=[25,25,65,85,105,35]
+
+let x
+
+x=arr.includes(105)
+console.log(x)
+x=arr.reverse()
+console.log(x)
+x=arr.sort();
+console.log(x)
+x=arr.slice(1)
+console.log(x,arr)
+x=arr.push(45)
+console.log(arr)
+x=arr.splice(2,6)
+console.log(x,arr)
