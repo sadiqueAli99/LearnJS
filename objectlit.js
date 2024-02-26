1d6ece27429f9494c1877512b7e2f692241281f9
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2024-02-26 10:51:37 +0400

    Object Litrelas

---
+++ objectlit.js
@@ -0,0 +1,17 @@
+const person={
+    name:"sadiue",
+    age:28,
+    isAdmin:true,
+    address:{ country:"UAE",
+    Emirates:"Dubai"
+
+    },
+    job:{
+        occup:"devloper"
+
+    }
+};
+
+console.log(person.address.Emirates);
+console.log(person.job.occup)
+console.log(person.address)
