1c3a6fe00dbea0a5de9ad1796f090cd36255ea92
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2024-02-27 07:49:45 +0400

    functions

---
+++ Functions/fnctn.js
@@ -0,0 +1,20 @@
+// Default param
+
+function user(name="Bot")
+{
+    return name + ' registerd sucessfully'
+}
+
+console.log(user('sadique'))
+
+//rset function
+
+function sum(...numbers){
+    let total=0;
+    for(const nums of numbers){
+        total+=nums
+    }
+    return total
+}
+
+console.log(sum(1,2,3,4))
